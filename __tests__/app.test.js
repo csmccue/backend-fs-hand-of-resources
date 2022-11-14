85deb1d93fd53dac019b117bd5a99a807f038e0f
Connor McCue <connor.mccue@comcast.net>
2022-11-13 23:36:15 -0800

    test 1 check for all car data

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,18 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('return all car data', async () => {
+    const res = await request(app).get('/cars');
+    expect(res.body.length).toEqual(10);
   });
+
   afterAll(() => {
     pool.end();
   });
