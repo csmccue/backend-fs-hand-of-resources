b5924b552c9e8c2e15c6706f2100e5d428621f6a
Connor McCue <connor.mccue@comcast.net>
2022-11-14 19:55:19 -0800

    resource 4 test 2

--- __tests__/drugs.tests.js
+++ __tests__/drugs.tests.js
@@ -7,10 +7,39 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('GET return all drugs data', async () => {
+  it.skip('GET return all drugs data', async () => {
     const res = await request(app).get('/drugs');
     expect(res.body.length).toEqual(10);
   });
+
+  it('GET return drugs 7', async () => {
+    const res = await request(app).get('/drugs/7');
+    const test = {
+      id: '7',
+      company: 'Antigen Laboratories, Inc.',
+      name: 'Treatment Set TS345381',
+      generic: 'Treatment Set TS345381',
+      fda: '49288-0847',
+      country: 'MG'
+    };
+    expect(res.body).toEqual(test);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   afterAll(() => {
     pool.end();
   });
