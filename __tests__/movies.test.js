b4aced25e43df08902733d148996902ee6235702
Connor McCue <connor.mccue@comcast.net>
2022-11-14 17:12:53 -0800

    resource 3 test 2 get movies/5 detail

--- __tests__/movies.test.js
+++ __tests__/movies.test.js
@@ -8,9 +8,24 @@ describe('backend-express-template routes', () => {
     return setup(pool);
   });
 
+  // test 1
   it('GET return all movies data', async () => {
     const res = await request(app).get('/movies');
     expect(res.body.length).toEqual(10);
+  });
+
+  // test 2 - GET movies 5 DETAIL
+  it('GET return movies 5', async () => {
+    const res = await request(app).get('/movies/5');
+    const test = {
+      id: '5',
+      title: 'Tunnel, The (Tunnel, Der)',
+      genre: 'Action|Drama|Thriller',
+      country: 'China',
+      director: 'Keslie Clausewitz',
+      language: 'Guaraní'
+    };
+    expect(res.body).toEqual(test);
   });
 
   afterAll(() => {
