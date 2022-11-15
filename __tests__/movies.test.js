364b897201bb1ef876b7ef153f8901e5fb0ec7fc
Connor McCue <connor.mccue@comcast.net>
2022-11-14 19:53:38 -0800

    resource 4 drugs test 1 pass

--- __tests__/movies.test.js
+++ __tests__/movies.test.js
@@ -9,13 +9,13 @@ describe('backend-express-template routes', () => {
   });
 
   // test 1
-  it('GET return all movies data', async () => {
+  it.skip('GET return all movies data', async () => {
     const res = await request(app).get('/movies');
     expect(res.body.length).toEqual(10);
   });
 
   // test 2 - GET movies 5 DETAIL
-  it('GET return movies 5', async () => {
+  it.skip('GET return movies 5', async () => {
     const res = await request(app).get('/movies/5');
     const test = {
       id: '5',
@@ -29,7 +29,7 @@ describe('backend-express-template routes', () => {
   });
 
   // test 3 - POST
-  it('POST should create new movie in data base', async () => {
+  it.skip('POST should create new movie in data base', async () => {
     const newMovie = {
       id: '11',
       title: 'Starship Troopers',
@@ -43,14 +43,14 @@ describe('backend-express-template routes', () => {
   });
 
   // test 4 - PUT
-  it('PUT /movies/6 should update with new director', async () => {
+  it.skip('PUT /movies/6 should update with new director', async () => {
     const resp = await request(app).put('/movies/6').send({ director: 'Emily' });
     expect(resp.status).toBe(200);
     expect(resp.body.director).toBe('Emily');
   });
 
   // test 5 - DELETE
-  it('DELETE /movies/4 should be deleted', async () => {
+  it.skip('DELETE /movies/4 should be deleted', async () => {
     const resp = await request(app).delete('/movies/4');
     expect(resp.status).toBe(204);
     const resp2 = await request(app).get('/movies/4');
